1f2980de17e82cd782c2d5c498d47d65d43b4bd3
theFONZ <blackthesoul@gmail.com>
2017-12-14 16:00:51 +0000

    small code logic changes
    Moved the actions to outside of the loop. All data collection is done inside the loop and passed out to the Set_Time method to do everything else

--- StreamDeckClock/Program.cs
+++ StreamDeckClock/Program.cs
@@ -58,34 +58,37 @@ namespace StreamDeckClock
                             //remove starting "0" from string
                             clock_hours = clock_hours.Remove(0, 1);
                         }
-                        //Set corresponding icon to the hour of day
-                        string clock_hours_icon = clock_hours + ".png";
-                        var clock_hours_bitmap = StreamDeckKeyBitmap.FromFile("clock_icons\\" + clock_hours_icon);
-                        deck.SetKeyBitmap(key_location_hours, clock_hours_bitmap);
+                        //send results to Set_Time to deal with
+                        Set_Time(clock_hours, key_location_hours);
                     }
 
                     //Check if minutes string is not empty
                     if (!string.IsNullOrEmpty(clock_minutes))
                     {
-                        //Set corresponding icon to the minutes of day
-                        string clock_minutes_icon = clock_minutes + ".png";
-                        var clock_minutes_bitmap = StreamDeckKeyBitmap.FromFile("clock_icons\\" + clock_minutes_icon);
-                        deck.SetKeyBitmap(key_location_minutes, clock_minutes_bitmap);
+                        //send results to Set_Time to deal with
+                        Set_Time(clock_minutes, key_location_minutes);
                     }
 
                     //Check if am/pm string is not empty
                     if (!string.IsNullOrEmpty(clock_ampm))
                     {
-                        //Set corresponding icon to am/pm
-                        string clock_ampm_icon = clock_ampm.ToLower() + ".png";
-                        var clock_ampm_bitmap = StreamDeckKeyBitmap.FromFile("clock_icons\\" + clock_ampm_icon);
-                        deck.SetKeyBitmap(key_location_ampm, clock_ampm_bitmap);
+                        //send results to Set_Time to deal with
+                        Set_Time(clock_ampm, key_location_ampm);
                     }
+
                     //Wait 1 second before restarting loop
                     System.Threading.Thread.Sleep(1000);
 
                     //Check for key presses, if pressed send exit command 
                     deck.KeyPressed += Deck_KeyPressed;
+                }
+
+                void Set_Time(string time_result, int key_location)
+                {
+                    //Set corresponding icons to the time of day
+                    string time_result_icon = time_result + ".png";
+                    var clock_bitmap = StreamDeckKeyBitmap.FromFile("clock_icons\\" + time_result_icon);
+                    deck.SetKeyBitmap(key_location, clock_bitmap);
                 }
 
                 void Deck_KeyPressed(object sender, StreamDeckKeyEventArgs e)
